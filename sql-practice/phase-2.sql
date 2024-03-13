ddbad02d2374c16c33194664ab4a12a44eaec5b1
FreemanAce1 <146105020+FreemanAce1@users.noreply.github.com>
2024-03-13 17:24:09 -0400

    Update phase-2.sql

--- sql-practice/phase-2.sql
+++ sql-practice/phase-2.sql
@@ -1,1 +1,20 @@
 -- Your code here 
+DROP customers IF EXISTS;
+DROP coffee_orders IF EXISTS;
+
+CREATE TABLE
+    customers (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(40) NOT NULL,
+        phone INTEGER UNIQUE CHECK (length (phone) = 10),
+        email VARCHAR(255) UNIQUE,
+        points INTEGER NOT NULL DEFAULT 5,
+        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIME
+    );
+
+CREATE TABLE
+    coffee_orders (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        is_redeemed BOOLEAN DEFAULT 0,
+        ordered_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIME
+    );
