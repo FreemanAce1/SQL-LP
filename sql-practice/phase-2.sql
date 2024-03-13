292cb0f7d4d15763e406b492fe10cbcbba9af57b
Timothy <tim.3imt@gmail.com>
2024-03-13 18:56:04 -0500

    at problem 11 phase 2

--- sql-practice/phase-2.sql
+++ sql-practice/phase-2.sql
@@ -1,10 +1,11 @@
--- Your code here 
-DROP customers IF EXISTS;
-DROP coffee_orders IF EXISTS;
+-- Your code here
+DROP TABLE IF EXISTS customers;
+
+DROP TABLE IF EXISTS coffee_orders;
 
 CREATE TABLE
     customers (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id INTEGER PRIMARY KEY,
         name VARCHAR(40) NOT NULL,
         phone INTEGER UNIQUE CHECK (length (phone) = 10),
         email VARCHAR(255) UNIQUE,
@@ -14,7 +15,7 @@ CREATE TABLE
 
 CREATE TABLE
     coffee_orders (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id INTEGER PRIMARY KEY,
         is_redeemed BOOLEAN DEFAULT 0,
         ordered_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIME
     );
--- sql-practice/phase-3.sql
+++ sql-practice/phase-3.sql
@@ -1,1 +1,86 @@
--- Your code here 
+-- Your code here
+INSERT INTO
+    customers (name, phone)
+VALUES
+    ('Rachel', 1111111111);
+
+UPDATE customers
+SET
+    points == 1
+WHERE
+    name = 'Rachel';
+
+INSERT INTO
+    coffee_orders DEFAULT
+VALUES;
+
+INSERT INTO
+    customers (name, email, phone)
+VALUES
+    ('Monica', 'monica@friends.show', 2222222222),
+    ('Phoebe', 'phoebe@friends.show', 3333333333);
+
+UPDATE customers
+SET
+    points = points + 3
+WHERE
+    name = 'Phoebe';
+
+INSERT INTO
+    coffee_orders DEFAULT
+VALUES;
+
+UPDATE customers
+SET
+    points = points + 4
+WHERE
+    name IN ('Rachel', 'Monica');
+
+INSERT INTO
+    coffee_orders DEFAULT
+VALUES;
+
+-- problem 6
+SELECT
+    points
+FROM
+    customers
+WHERE
+    name = 'Monica';
+
+-- problem 7
+SELECT
+    points
+FROM
+    customers
+WHERE
+    name = 'Rachel';
+
+UPDATE customers
+SET
+    points = 0
+WHERE
+    name = 'Rachel';
+
+INSERT INTO
+    coffee_orders (is_redeemed)
+VALUES
+    (1);
+
+-- problem 8
+INSERT INTO
+    customers (name, email)
+VALUES
+    ('Joey', 'joey@friends.show'),
+    ('Chandler', 'chandler@friends.show'),
+    ('Ross', 'ross@friends.show');
+
+-- problem 9
+INSERT INTO
+    coffee_orders DEFAULT
+VALUES;
+
+-- problem 10
+INSERT INTO
+    coffee_orders DEFAULT
+VALUES;
